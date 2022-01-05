750680000f0c35e30a6540d622eba0f467127784
lin <lin@ziaozhang.com>
2022-01-06 06:52:01 +0800

    third commit

--- src/main/java/com/lin/Test.java
+++ src/main/java/com/lin/Test.java
@@ -3,7 +3,9 @@ package com.lin;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
-        System.out.println("Hello,Git");
-        System.out.println("Hello,Git");
+        System.out.println("Hello,Git2");
+        System.out.println("Hello,Git3");
+        System.out.println("Hello,Git4");
+        System.out.println("Hello,Git5");
     }
 }
