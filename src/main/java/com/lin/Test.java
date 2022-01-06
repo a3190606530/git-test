0fdf92fa134d9bf6da2970dea644184352a44298
a3190606530 <91442165+a3190606530@users.noreply.github.com>
2022-01-06 11:48:13 +0800

    pull commit

--- src/main/java/com/lin/Test.java
+++ src/main/java/com/lin/Test.java
@@ -10,5 +10,6 @@ public class Test {
         System.out.println("master  test");
         System.out.println("hots-fix test222!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
